ab42d00fdda7e2b0ec2d92af6d28d7e6b4093c50
dvvento <lalkegs@gmail.com>
2025-10-24 16:22:24 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let calen = prompt('напиши число недели')
+
+switch (calen) {
+    case '1' :
+        alert('понидельник')
+        break;
+    case '2' :
+        alert('вторник')
+        break;
+    case '3' :
+        alert('среда')
+        break;
+    case '4' :
+        alert('четверг')
+        break;
+    case '5' :
+        alert('пятница')
+        break;
+    case '6' :
+        alert('суботта')
+        break;
+    case '7' :
+        alert('восресенье')
+        break;
+    default:
+        alert('напиши число недели!')
+        break;
+}
